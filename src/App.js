3c0bfb20e3315f30f87dbac4236db3f2ce3511dc
OceanPR <oceanprincessit@gmail.com>
2022-01-12 16:40:29 +0630

    menu bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import './font.css';
+import { Divide as Hamburger } from 'hamburger-react';
+
 class App extends React.Component {
+
+constructor () {
+    super()
+    this.state = {
+      isHidden: true
+    }
+  }
+
+toggleHidden () {
+    this.setState({
+      isHidden: !this.state.isHidden
+     
+    })
+     console.log(this.state.isHidden);
+  }
+
 render() {
 return (
   <div>
-      <div className="p-2 fixed">
+        {!this.state.isHidden && <div  className="w-full h-screen bg-black fixed">
+        acd
+        </div> }
+      <div className="p-2 fixed z-4" >
       <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
             <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
               <div className="flex items-center justify-between w-full md:w-auto">
                 
-                <div class="-mr-2 flex items-center md:hidden">
-                  <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-black hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
+                <div className="-mr-2 flex items-center md:hidden">
+                  <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-black hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
+                    <span className="sr-only">Open main menu</span>
                     
-                    <svg className="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
+                    
+                    <span onClick={this.toggleHidden.bind(this)}><Hamburger rounded /></span>
                   </button>
                 </div>
               </div>
@@ -33,43 +53,76 @@ return (
             </div>
           </nav>
         </div>
+        
         <div className="px-[10%] py-[70%] bg-[url('../public/mbBg1.png')] min-h-screen max-h-screen bg-cover border">
-          <h1 className=" text-3xl font-medium my-[10%]">Hello Nice To Meet Ya!</h1>
-          <p className=" text-sm">I'm Krost of the Burma. I'm a developer, a coder and also a UI/UX designer. Thanks for visiting my Portfolio. </p>
-          <p className=" mx-[35%] mt-[200px] text-sm text-center">Explore More</p>
+          <h1 className=" text-4xl font-semibold my-[10%]">Hello <br/>Nice To Meet Ya!</h1>
+          <p className=" text-base">I'm Krost of the Burma. I'm a developer, a coder and also a UI/UX designer. Thanks for visiting my Portfolio. </p>
+          <p className=" mx-[40%] mt-[100px] text-sm text-center border p-2 h-[40px] w-[100px] rounded-full text-cyan-600 border-sky-600">Explore More</p>
         </div>
-        <div className=" bg-[url('../public/mbBg2.png')] min-h-screen bg-cover px-[10%] py-[100%] max-h-screen">
-          <p className=" text-sm text-center">I'm Krost of the Burma. I'm a developer, a coder and also a UI/UX designer. Thanks for visiting my Portfolio. So when you were here, I would like to suggest you take a tour my portfolio and take a look to my design. There's are a bunch of code and if you want you can copy my code. But make sure you give the credit for me. And if you don't understand my code you can contact me by hitting contact button. Well then, take a tour.
-I promise you won't regret it. </p>
+        <div className=" bg-[url('../public/mbBg2.png')] min-h-screen bg-cover px-[10%] py-[30%] max-h-screen">
+          <h1 className=" text-4xl font-semibold mb-[22%] mx-auto">Who Am I?</h1>
+          <p className=" text-lg my-2">I'm Krost of the Burma. I'm a developer, a coder and also a UI/UX designer. Thanks for visiting my Portfolio. So when you were here, I would like to suggest you take a tour my portfolio and take a look to my design.  Well then, take a tour.
+            I promise you won't regret it. </p>
+          <p className="  mt-[100px] text-sm text-center border p-2 h-[40px] w-[150px] text-cyan-800 border-teal-500">More About Me</p>
         </div>
-        <div className=" bg-[url('../public/mbBg3.png')] min-h-screen bg-cover px-[10%] py-[20%] max-h-screen border-b">
-          <h1 className=" text-4xl font-medium my-[10%] text-white">My Works</h1>
-            <div className="grid grid-rows-3 grid-flow-col gap-4 h-[165px]">
-                <div className="row-start-1 row-end-4 w-[150px] border rounded-lg">
+        <div className=" bg-[url('../public/mbBg3.png')] min-h-screen bg-cover py-[20%] max-h-screen border-b px-2 overflow-hidden">
+          <h1 className=" text-4xl font-medium mb-[30%] text-white mx-[5%]">My Works</h1>
+            <div className="grid grid-rows-3 grid-flow-col gap-4">
+                <div className="row-start-1 row-end-4 border rounded-lg">
                   <img src="https://financialallianceforwomen.org/wp-content/uploads/2020/09/HAckathon-website-thumbnail-.png" className="rounded-lg min-h-[100%] max-h-[100%] min-w-[100%] max-w[100%]"/>
                 </div>
-                <div className="row-start-1 row-end-4 w-[150px] border rounded-lg">
+                <div className="row-start-1 row-end-4 border rounded-lg">
                   <img src="https://500px.com/staticV2/media/editors8@1x.126c6fb9.png" className="rounded-lg min-h-[100%] max-h-[100%] min-w-[100%] max-w[100%]"/>
                 </div>
             </div>
-            <div class="container border h-[200px] rounded-lg mt-4">
+            <div className="container border h-[200px] rounded-lg mt-4">
               <img src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5e6e26d9aa5428000759e96c%2Fwomen-washing-purple-water-lilies-in-Vietnam%2F960x0.jpg%3FcropX1%3D0%26cropX2%3D960%26cropY1%3D112%26cropY2%3D954" className="rounded-lg  min-h-[100%] max-h-[100%] min-w-[100%] max-w[100%] "/>
             </div>
-            <div className="grid grid-rows-3 grid-flow-col gap-4 h-[165px] mt-4">
-                <div className="row-start-1 row-end-4 w-[150px] border rounded-lg">
-                  <img src="https://image.freepik.com/free-photo/inspired-caucasian-female-model-with-short-haircut-looking-away-with-shy-smile-purple-wall-photo-lovable-brown-haired-woman-sweater-relaxing-photoshoot_197531-7990.jpg" className="rounded-lg min-h-[100%] max-h-[100%] min-w-[100%] max-w[100%]"/>
-                </div>
-                <div className="row-start-1 row-end-4 w-[150px] border rounded-lg">
-                  <img src="https://www.oberlo.com/media/1603954923-image23-1.png?w=1824&fit=max" className="rounded-lg min-h-[100%] max-h-[100%] min-w-[100%] max-w[100%]"/>
-                </div>
-            </div>
-            <svg className="float-right my-2"width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" fill="#ffffff"/></svg>
+            <svg className="float-right my-2"width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" fill="#ffffff"/></svg>
         </div>
-        <div className=" bg-[url('../public/mbBg4.png')] min-h-screen bg-cover max-h-screen px-[10%] py-[20%]">
-            <div class="container border h-[200px] rounded-lg mt-20">
-              <img src="https://cvinfos.com/wp-content/uploads/2020/03/00-10-scaled.jpg" className="rounded-lg "/>
-
+        <div className=" bg-[url('../public/mbBg4.png')] min-h-screen bg-cover max-h-screen px-[10%] py-[5%]">
+            <div className="container  h-[400px] rounded-lg mt-10">
+              <img src="https://cvinfos.com/wp-content/uploads/2020/03/00-10-scaled.jpg" className="rounded-lg max-h-[100%] mx-auto"/>
+                  <ul className="my-4 mx-[10%]"> 
+                  <li className="flex"> 
+                    <span className="text-sm mr-2">Html/Css</span>
+                    <div className="w-full bg-gray-200 h-1 mt-2">
+                      <div className="bg-blue-600 h-1 w-[75%]"></div>
+                    </div>
+                  </li>
+                  <li className="flex"> 
+                    <span className="text-sm mr-2">Html/Css</span>
+                    <div className="w-full bg-gray-200 h-1 mt-2">
+                      <div className="bg-blue-600 h-1 w-[75%]"></div>
+                    </div>
+                  </li>
+                  <li className="flex"> 
+                    <span className="text-sm mr-2">Html/Css</span>
+                    <div className="w-full bg-gray-200 h-1 mt-2">
+                      <div className="bg-blue-600 h-1 w-[75%]"></div>
+                    </div>
+                  </li>
+                  <li className="flex"> 
+                    <span className="text-sm mr-2">Html/Css</span>
+                    <div className="w-full bg-gray-200 h-1 mt-2">
+                      <div className="bg-blue-600 h-1 w-[75%]"></div>
+                    </div>
+                  </li>
+                  <li className="flex"> 
+                    <span className="text-sm mr-2">Html/Css</span>
+                    <div className="w-full bg-gray-200 h-1 mt-2">
+                      <div className="bg-blue-600 h-1 w-[75%]"></div>
+                    </div>
+                  </li>
+                  <li className="flex"> 
+                    <span className="text-sm mr-2">Html/Css</span>
+                    <div className="w-full bg-gray-200 h-1 mt-2">
+                      <div className="bg-blue-600 h-1 w-[75%]"></div>
+                    </div>
+                  </li>
+                  </ul>
             </div>
+
         </div>
       </div>);
 }
